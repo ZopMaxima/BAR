e5fb76c44004e7e921df4ee3976a34c112f28ee8
ZopMaxima <max.jackman@outlook.com>
2025-11-20 17:45:12 -0700

    Revert to original.

--- Tweaks_Unused/DjarshiT3Pack.lua
+++ Tweaks_Unused/DjarshiT3Pack.lua
@@ -1,38 +1,38 @@
---LavaT3pack Djarshi v4.8.1
+-- LavaT3pack Djarshi v4.8
 local races = {"arm", "cor", "leg"}
 local a, b, c, d, e, f, g =
-	UnitDefs or {},
-	{"arm", "cor", "leg"},
-	table.merge,
-	{arm = "Armada ", cor = "Cortex ", leg = "Legion "},
-	"_taxed",
-	1.5,
-	table.contains
+    UnitDefs or {},
+    {"arm", "cor", "leg"},
+    table.merge,
+    {arm = "Armada ", cor = "Cortex ", leg = "Legion "},
+    "_taxed",
+    1.5,
+    table.contains
 
 local function h(i, j, k)
-	if a[i] and not a[j] then
-		a[j] = c(a[i], k)
-	end
+    if a[i] and not a[j] then
+        a[j] = c(a[i], k)
+    end
 end
 
 local function addC(conName,newUnit)
-	if UnitDefs[conName] and UnitDefs[conName].buildoptions and not table.contains(UnitDefs[conName].buildoptions, newUnit) then
-		table.insert(UnitDefs[conName].buildoptions, newUnit)
-	end
+    if UnitDefs[conName] and UnitDefs[conName].buildoptions and not table.contains(UnitDefs[conName].buildoptions, newUnit) then
+        table.insert(UnitDefs[conName].buildoptions, newUnit)
+    end
 end
 
 local function addUnitToBO(newUnit, ...)
-	local rest = {...}
-	for i, v in ipairs(rest) do
-		addC( v, newUnit )
-	end
+    local rest = {...}
+    for i, v in ipairs(rest) do
+        addC( v, newUnit )
+    end
 end
 
 local function mergeToNew(u, newU, obj)
-	if UnitDefs[u] and not UnitDefs[newU] then
-		UnitDefs[newU] = table.merge(UnitDefs[u], obj)
-	end
-	return UnitDefs[newU]
+    if UnitDefs[u] and not UnitDefs[newU] then
+        UnitDefs[newU] = table.merge(UnitDefs[u], obj)
+    end
+    return UnitDefs[newU]
 end
 
 local ym = 'h cbbybjyybc bjbjjbbjjb yjbjbjjbbb ybjjjbjjjy jbjbjjjbjb bjbjjjbjbj yjjjbjjjby bbbjjbjbjy bjjbbjjbjb cbyyjbybbc'
@@ -41,209 +41,207 @@ UnitDefs['corageo'].yardmap = ym
 UnitDefs['armuwageo'].yardmap = ym
 UnitDefs['coruwageo'].yardmap = ym
 if ( UnitDefs['legageo'] ) then
-	UnitDefs['legageo'].yardmap = ym
+    UnitDefs['legageo'].yardmap = ym
 end
 
 for l, m in pairs(b) do
-	local n, o, p = m == "arm", m == "cor", m == "leg"
-	h(
-		m .. "nanotct2",
-		m .. "nanotct3",
-		{
-			icontype = "armrespawn",
-			metalcost = 3360,
-			energycost = 51200,
-			builddistance = 625,
-			buildtime = 88000,
-			collisionvolumescales = "61 128 61",
-			footprintx = 6,
-			footprintz = 6,
-			health = 8800,
-			mass = 37200,
-			sightdistance = 625,
-			workertime = 3000,
-			canrepeat = true,
-			objectname = p and "Units/legnanotcbase.s3o" or o and "Units/CORRESPAWN.s3o" or "Units/ARMRESPAWN.s3o",
-			customparams = {
-				i18n_en_humanname = "Epic Construction Turret",
-				i18n_en_tooltip = "Maximum BUILDPOWER! (Ripped from NuttyB, tweaked by Djarshi & txpera)"
-			}
-		}
-	)
-	h(
-		m .. "ageo",
-		m .. "ageot3",
-		{
-			icontype = "armuwageo",
-			buildtime = 88000,
-			collisionvolumeoffsets = "0 0 0",
-			collisionvolumescales = "61 128 61",
-			energycost = 270000,
-			energymake = 12500,
-			energystorage = 120000,
-			footprintx = 7,
-			footprintz = 7,
-			health = 7120,
-			idleautoheal = 33,
-			idletime = 1800,
-			maxacc = 0,
-			maxdec = 0,
-			maxslope = 15,
-			maxwaterdepth = 5,
-			metalcost = 16000,
-			objectname = "Units/mission_command_tower.s3o",
-			buildpic = "scavengers/mission_command_tower.dds",
-			script = "Units/ARMAGEO.cob",
-			seismicsignature = 0,
-			selfdestructas = "advgeo",
-			-- extended from 'ym' of other geo's
-			yardmap = "h oooooooooooooo oooooooooooooo oocbgybsyybcoo oobsbssbbssboo ooysbsbssbbgoo ooybsssbsssyoo oosbsbsssbsboo oobsbsssbsbsoo ooysssbsssbyoo oogbbssbsbsyoo oobssbbssbsboo oocbyysbygbcoo oooooooooooooo oooooooooooooo",
-			sightdistance = 345,
-			customparams = {
-				i18n_en_humanname = "Epic Geothermal Powerplant",
-				i18n_en_tooltip = "Produces 10x T2 Geothermal + has plasma deflector. (tweaked by Djarshi & txpera)",
-				shield_color_mult = 0.99,
-				shield_power = 3250,
-				shield_radius = 750
-			},
-			weapondefs = {
-				repulsor = {
-					avoidfeature = false,
-					craterareaofeffect = 0,
-					craterboost = 0,
-					cratermult = 0,
-					edgeeffectiveness = 0.15,
-					name = "PlasmaRepulsor",
-					soundhitwet = "sizzle",
-					weapontype = "Shield",
-					shield = {
-						alpha = 0.17,
-						armortype = "shields",
-						energyupkeep = 0,
-						force = 2.5,
-						intercepttype = 1,
-						power = 6700,
-						powerregen = 69,
-						powerregenenergy = 562.5,
-						radius = 750,
-						repulser = true,
-						smart = true,
-						startingpower = 1100,
-						visiblerepulse = true,
-						badcolor = {
-							[1] = 1,
-							[2] = 0.2,
-							[3] = 0.2,
-							[4] = 0.2,
-						},
-						goodcolor = {
-							[1] = 0.2,
-							[2] = 1,
-							[3] = 0.2,
-							[4] = 0.17,
-						},
-					},
-				},
-			},
-			weapons = {
-				[1] = {
-					def = "REPULSOR",
-					onlytargetcategory = "NOTSUB",
-				},
-			}
-		}
-	)
+    local n, o, p = m == "arm", m == "cor", m == "leg"
+    h(
+        m .. "nanotct2",
+        m .. "nanotct3",
+        {
+            metalcost = 3360,
+            energycost = 51200,
+            builddistance = 625,
+            buildtime = 88000,
+            collisionvolumescales = "61 128 61",
+            footprintx = 6,
+            footprintz = 6,
+            health = 8800,
+            mass = 37200,
+            sightdistance = 625,
+            workertime = 3000,
+            canrepeat = true,
+            objectname = p and "Units/legnanotcbase.s3o" or o and "Units/CORRESPAWN.s3o" or "Units/ARMRESPAWN.s3o",
+            customparams = {
+                i18n_en_humanname = "Epic Construction Turret",
+                i18n_en_tooltip = "Maximum BUILDPOWER! (Ripped from NuttyB, tweaked by Djarshi & txpera)"
+            }
+        }
+    )
+    h(
+        m .. "ageo",
+        m .. "ageot3",
+        {
+            buildtime = 88000,
+		    collisionvolumeoffsets = "0 0 0",
+		    collisionvolumescales = "61 128 61",
+		    energycost = 270000,
+		    energymake = 12500,
+		    energystorage = 120000,
+		    footprintx = 7,
+		    footprintz = 7,
+		    health = 7120,
+		    idleautoheal = 33,
+		    idletime = 1800,
+		    maxacc = 0,
+		    maxdec = 0,
+		    maxslope = 15,
+		    maxwaterdepth = 5,
+		    metalcost = 16000,
+            objectname = "Units/mission_command_tower.s3o",
+            buildpic = "scavengers/mission_command_tower.dds",
+		    script = "Units/ARMAGEO.cob",
+		    seismicsignature = 0,
+		    selfdestructas = "advgeo",
+            -- extended from 'ym' of other geo's
+            yardmap = "h oooooooooooooo oooooooooooooo oocbgybsyybcoo oobsbssbbssboo ooysbsbssbbgoo ooybsssbsssyoo oosbsbsssbsboo oobsbsssbsbsoo ooysssbsssbyoo oogbbssbsbsyoo oobssbbssbsboo oocbyysbygbcoo oooooooooooooo oooooooooooooo",
+		    sightdistance = 345,
+            customparams = {
+                i18n_en_humanname = "Epic Geothermal Powerplant",
+                i18n_en_tooltip = "Produces 10x T2 Geothermal + has plasma deflector. (tweaked by Djarshi & txpera)",
+    			shield_color_mult = 0.99,
+	    		shield_power = 3250,
+	    		shield_radius = 750
+            },
+            weapondefs = {
+	            repulsor = {
+		            avoidfeature = false,
+		            craterareaofeffect = 0,
+		            craterboost = 0,
+		            cratermult = 0,
+		            edgeeffectiveness = 0.15,
+		            name = "PlasmaRepulsor",
+		            soundhitwet = "sizzle",
+		            weapontype = "Shield",
+		            shield = {
+			            alpha = 0.17,
+			            armortype = "shields",
+			            energyupkeep = 0,
+			            force = 2.5,
+			            intercepttype = 1,
+			            power = 6700,
+			            powerregen = 69,
+			            powerregenenergy = 562.5,
+			            radius = 750,
+			            repulser = true,
+			            smart = true,
+			            startingpower = 1100,
+			            visiblerepulse = true,
+			            badcolor = {
+				            [1] = 1,
+				            [2] = 0.2,
+				            [3] = 0.2,
+				            [4] = 0.2,
+			            },
+			            goodcolor = {
+				            [1] = 0.2,
+				            [2] = 1,
+				            [3] = 0.2,
+				            [4] = 0.17,
+			            },
+		            },
+	            },
+            },
+            weapons = {
+	            [1] = {
+		            def = "REPULSOR",
+		            onlytargetcategory = "NOTSUB",
+	            },
+            }
+        }
+    )
 
-	for l, q in pairs({m .. "nanotc", m .. "nanotct2"}) do
-		if a[q] then
-			a[q].canrepeat = true
-		end
-	end
+    for l, q in pairs({m .. "nanotc", m .. "nanotct2"}) do
+        if a[q] then
+            a[q].canrepeat = true
+        end
+    end
 
-	local AFU = m .. "afust3"
-	if a[AFU] then
-		a[AFU].explodeas = "customfusionexplo"
-		a[AFU].selfdestructas = "advancedFusionExplosionSelfd"
-	end
+    local AFU = m .. "afust3"
+    if a[AFU] then
+    	a[AFU].explodeas = "customfusionexplo"
+    	a[AFU].selfdestructas = "advancedFusionExplosionSelfd"
+    end
 end
 
 local newAfus = mergeToNew( 
-		"lootboxplatinum",
-		"afuslegendary",
-		{
-			icontype = "armafus",
-			buildpic = "other/resourcecheat.dds",
-			buildtime = 120000,
-			metalmake = 0,
-			footprintx = 12,
-			footprintz = 12,
-			explodeas = "ScavComBossExplo",
-			reclaimable = true,
-			customparams = {
-				i18n_en_humanname = "Legendary Fusion Reactor",
-				i18n_en_tooltip = "Makes 50x of AFUS, Transportable, Unique (Very Hazardous)",
-				shield_color_mult = 0.99,
-				shield_power = 56000,
-				shield_radius = 1250,
-				fall_damage_multiplier = 15,
-			},
-			weapondefs = {
-				repulsor = {
-					avoidfeature = false,
-					craterareaofeffect = 0,
-					craterboost = 0,
-					cratermult = 0,
-					edgeeffectiveness = 0.15,
-					name = "PlasmaRepulsor",
-					soundhitwet = "sizzle",
-					weapontype = "Shield",
-					shield = {
-						alpha = 0.25,
-						armortype = "shields",
-						energyupkeep = 0,
-						force = 2.5,
-						intercepttype = 1,
-						power = 56000,
-						powerregen = 1300,
-						powerregenenergy = 100000,
-						radius = 1250,
-						repulser = true,
-						smart = true,
-						startingpower = 1,
-						visiblerepulse = true,
-						badcolor = {
-							[1] = 1,
-							[2] = 0.1,
-							[3] = 0.1,
-							[4] = 0.1,
-						},
-						goodcolor = {
-							[1] = 0.1,
-							[2] = 1,
-							[3] = 0.1,
-							[4] = 0.1,
-						},
-					},
-				},
-			},
-			weapons = {
-				[1] = {
-					def = "REPULSOR",
-					onlytargetcategory = "NOTSUB",
-				},
-			},
-		}
-	)
+        "lootboxplatinum",
+        "afuslegendary",
+        {
+            buildpic = "other/resourcecheat.dds",
+            buildtime = 120000,
+            metalmake = 0,
+            footprintx = 12,
+            footprintz = 12,
+            explodeas = "ScavComBossExplo",
+            reclaimable = true,
+            customparams = {
+                i18n_en_humanname = "Legendary Fusion Reactor",
+                i18n_en_tooltip = "Makes 50x of AFUS, Transportable, Unique (Very Hazardous)",
+                shield_color_mult = 0.99,
+                shield_power = 56000,
+                shield_radius = 1250,
+                fall_damage_multiplier = 15,
+            },
+            weapondefs = {
+                repulsor = {
+                    avoidfeature = false,
+                    craterareaofeffect = 0,
+                    craterboost = 0,
+                    cratermult = 0,
+                    edgeeffectiveness = 0.15,
+                    name = "PlasmaRepulsor",
+                    soundhitwet = "sizzle",
+                    weapontype = "Shield",
+                    shield = {
+                        alpha = 0.25,
+                        armortype = "shields",
+                        energyupkeep = 0,
+                        force = 2.5,
+                        intercepttype = 1,
+                        power = 56000,
+                        powerregen = 1300,
+                        powerregenenergy = 100000,
+                        radius = 1250,
+                        repulser = true,
+                        smart = true,
+                        startingpower = 1,
+                        visiblerepulse = true,
+                        badcolor = {
+                            [1] = 1,
+                            [2] = 0.1,
+                            [3] = 0.1,
+                            [4] = 0.1,
+                        },
+                        goodcolor = {
+                            [1] = 0.1,
+                            [2] = 1,
+                            [3] = 0.1,
+                            [4] = 0.1,
+                        },
+                    },
+                },
+            },
+            weapons = {
+                [1] = {
+                    def = "REPULSOR",
+                    onlytargetcategory = "NOTSUB",
+                },
+            },
+        }
+    )
 
-	local normAfus = UnitDefs[ "armafus"]
-	if ( normAfus ) then
-		newAfus.metalcost = normAfus.metalcost*30
-		newAfus.energycost = normAfus.energycost*30
-		newAfus.energymake = normAfus.energymake*50
-		newAfus.energystorage = normAfus.energystorage*50
-		newAfus.health = normAfus.health*10
-		newAfus.maxthisunit=1
-	end
+    local normAfus = UnitDefs[ "armafus"]
+    if ( normAfus ) then
+        newAfus.metalcost = normAfus.metalcost*30
+        newAfus.energycost = normAfus.energycost*30
+        newAfus.energymake = normAfus.energymake*50
+        newAfus.energystorage = normAfus.energystorage*50
+        newAfus.health = normAfus.health*10
+        newAfus.maxthisunit=1
+    end
+
 
 addUnitToBO( "afuslegendary", "armack", "armaca", "armacv" )
 addUnitToBO( "afuslegendary", "corack", "coraca", "coracv" )
@@ -251,30 +249,29 @@ addUnitToBO( "afuslegendary", "legack", "legaca", "legacv" )
 
 -- t3mex
 local newT3Mex = mergeToNew( 
-		"armmoho",
-		"t3mmex",
-		{
-			icontype = "armmoho",
-			health = 6200,
-			metalstorage = 2000,
-			buildpic = "scavengers/scavsafeareabeacon.DDS",
-			buildtime = 30000,
-			reclaimable = true,
-			objectname = "scavs/scavsafeareabeacon.s3o",
-			script = "Units/ARMEYES.cob",
-			energycost = 24300,
-			metalcost = 1920,
-			energyupkeep = 500,
-			explodeas = "geo",
-			extractsmetal = 0.016,
-			onoffable = true,
-			yardmap = "h oooooooo osssssso osssssso ossoosso ossoosso osssssso osssssso oooooooo",
-			customparams = {
-				i18n_en_humanname = "Epic Metal Extractor",
-				i18n_en_tooltip = "Metal Extraction / Storage (OP by FiendishDevil)",
-			},
-		}
-	)
+        "armmoho",
+        "t3mmex",
+        {
+            icontype = "legmex",
+            health = 6200,
+            metalstorage = 2000,
+            buildpic = "scavengers/scavsafeareabeacon.DDS",
+            buildtime = 30000,
+            reclaimable = true,
+            objectname = "scavs/scavsafeareabeacon.s3o",
+            energycost = 24300,
+            metalcost = 1920,
+            energyupkeep = 500,
+            explodeas = "geo",
+            extractsmetal = 0.016,
+            onoffable = true,
+            yardmap = "h oooooooo osssssso osssssso ossoosso ossoosso osssssso osssssso oooooooo",
+            customparams = {
+                i18n_en_humanname = "Epic Metal Extractor",
+                i18n_en_tooltip = "Metal Extraction / Storage (OP by FiendishDevil)",
+            },
+        }
+    )
 
 addUnitToBO( "t3mmex", "armack", "armaca", "armacv" )
 addUnitToBO( "t3mmex", "corack", "coraca", "coracv" )
@@ -282,12 +279,21 @@ addUnitToBO( "t3mmex", "legack", "legaca", "legacv" )
 
 -- converter size fix
 for l, m in pairs({ "armmmkrt3", "cormmkrt3", "legadveconvt3" }) do
-	if ( UnitDefs[m] ) then
-		UnitDefs[m] = table.merge(UnitDefs[m], { footprintx = 6, footprintz = 6 })
-	end
+    if ( UnitDefs[m] ) then
+        UnitDefs[m] = table.merge(UnitDefs[m], { footprintx = 6, footprintz = 6 })
+    end
 end
 
+
+
 UnitDefs["corjugg"].metalcost=UnitDefs["corjugg"].metalcost*2
+UnitDefs["legeheatraymech"].metalcost=UnitDefs["legeheatraymech"].metalcost*2
+if (UnitDefs["legeheatraymech_old"]) then
+    UnitDefs["legeheatraymech_old"].metalcost=UnitDefs["legeheatraymech_old"].metalcost*2
+end
+
+
+
 
 UnitDefs["armsy"].health = 0
 UnitDefs["armasy"].maxthisunit = 0
@@ -305,3 +311,10 @@ addUnitToBO( "cornanotct3", "corack", "coraca", "coracv" )
 addUnitToBO( "corageot3", "corack", "coraca", "coracv" )
 addUnitToBO( "legnanotct3", "legack", "legaca", "legacv" )
 addUnitToBO( "legageot3", "legack", "legaca", "legacv" )
+
+
+UnitDefs["armvulc"].metalcost=UnitDefs["armvulc"].metalcost*10
+UnitDefs["corbuzz"].metalcost=UnitDefs["corbuzz"].metalcost*10
+UnitDefs["legstarfall"].metalcost=UnitDefs["legstarfall"].metalcost*10
+
+
